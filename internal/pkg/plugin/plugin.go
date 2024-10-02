4d55abe60fd5dafecdebe16477bee675b8e0b4cb
nialdaly <nial.daly@outlook.com>
2024-10-02 18:07:13 +0100

    reverted TrustZonesGRPCClient

--- internal/pkg/plugin/plugin.go
+++ internal/pkg/plugin/plugin.go
@@ -3,7 +3,6 @@ package plugin
 import (
 	"context"
 
-	"github.com/cofide/cofide-connect/pkg/api"
 	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
 )
@@ -11,10 +10,6 @@ import (
 type DataSourcePlugin struct {
 	plugin.Plugin
 	Impl DataSource
-}
-
-type TrustZonesGRPCClient struct {
-	client api.TrustZoneServiceClient
 }
 
 func (p *DataSourcePlugin) ConnectDataSourceGRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
