28a0e84c98bc0ba6967e6fff5f3c65b907516de4
nialdaly <nial.daly@outlook.com>
2024-10-02 19:21:40 +0100

    additional changes post grpc protos

---
+++ internal/pkg/plugin/grpc.go
@@ -0,0 +1,32 @@
+package plugin
+
+import (
+	"context"
+
+	cofidectl_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/cofidectl_plugin/v1"
+
+	trust_zone_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/trust_zone/v1"
+)
+
+type ConnectDataSourceGRPCClient struct {
+	client cofidectl_proto.DataSourcePluginServiceClient
+}
+
+func (c *ConnectDataSourceGRPCClient) GetTrustZones() ([]*trust_zone_proto.TrustZone, error) {
+
+	resp, err := c.client.GetTrustZones(context.Background(), &cofidectl_proto.GetTrustZonesRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.TrustZones, nil
+}
+
+type ConnectDataSourceGRPCServer struct {
+	Impl DataSource
+}
+
+func (c *ConnectDataSourceGRPCServer) GetTrustZones(context.Context, *cofidectl_proto.GetTrustZonesRequest) (*cofidectl_proto.GetTrustZonesResponse, error) {
+	v, err := c.Impl.GetTrustZones()
+	return &cofidectl_proto.GetTrustZonesResponse{TrustZones: v}, err
+}
--- internal/pkg/plugin/plugin.go
+++ internal/pkg/plugin/plugin.go
@@ -3,6 +3,7 @@ package plugin
 import (
 	"context"
 
+	cofidectl_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/cofidectl_plugin/v1"
 	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
 )
@@ -13,9 +14,10 @@ type DataSourcePlugin struct {
 }
 
 func (p *DataSourcePlugin) ConnectDataSourceGRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
+	cofidectl_proto.RegisterDataSourcePluginServiceServer(s, &ConnectDataSourceGRPCServer{Impl: p.Impl})
 	return nil
 }
 
 func (p *DataSourcePlugin) ConnectDataSourceGRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
-	return nil, nil
+	return &ConnectDataSourceGRPCClient{client: cofidectl_proto.NewDataSourcePluginServiceClient(c)}, nil
 }
