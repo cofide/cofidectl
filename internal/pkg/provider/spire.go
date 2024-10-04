6dc570296c8a79672fd2f47cab082938090de4ae
Jason Costello <jason@cofide.io>
2024-10-04 16:44:25 +0100

    Skeleton of SPIREProvider

---
+++ internal/pkg/provider/spire.go
@@ -0,0 +1,39 @@
+package provider
+
+import (
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/release"
+)
+
+// SPIREProvider is the interface to downstream SPIRE installation methodologies for the Cofide stack
+type SPIREProvider interface {
+	Execute() error
+}
+
+// HelmSPIREProvider implements a Helm-based installer for the Cofide stack
+type HelmSPIREProvider struct {
+	chart   string
+	version string
+}
+
+func NewHelmSPIREProvider(chart string, version string) *HelmSPIREProvider {
+	return &HelmSPIREProvider{
+		chart:   chart,
+		version: version,
+	}
+}
+
+func (h *HelmSPIREProvider) Execute() {
+	h.newInstall()
+}
+
+func (h *HelmSPIREProvider) newInstall() (*release.Release, error) {
+	actionConfig := new(action.Configuration)
+	client := action.NewInstall(actionConfig)
+	client.Version = h.version
+
+	options, _ := client.ChartPathOptions.LocateChart("spire/spire", nil)
+	cr, _ := loader.Load(options)
+	return client.Run(cr, nil)
+}
