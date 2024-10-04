676e5e48116e81f7fc90fa5f7ea049884fe0d570
Matthew Bates <matt@cofide.io>
2024-10-03 21:22:55 -0400

    remove server here

--- pkg/plugin/grpc.go
+++ pkg/plugin/grpc.go
@@ -3,7 +3,6 @@ package plugin
 import (
 	"context"
 
-	attestation_policy_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/attestation_policy/v1"
 	cofidectl_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/cofidectl_plugin/v1"
 
 	trust_zone_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/trust_zone/v1"
@@ -24,6 +23,8 @@ func (c *DataSourcePluginClientGRPC) GetTrustZones() ([]*trust_zone_proto.TrustZ
 	return resp.TrustZones, nil
 }
 
+/*
+
 func (c *DataSourcePluginClientGRPC) CreateTrustZone() (*trust_zone_proto.TrustZone, error) {
 	resp, err := c.client.CreateTrustZone(context.Background(), &cofidectl_proto.CreateTrustZoneRequest{})
 	if err != nil {
@@ -41,24 +42,4 @@ func (c *DataSourcePluginClientGRPC) CreateAttestationPolicy() (*attestation_pol
 
 	return resp.AttestationPolicy, nil
 }
-
-// DataSourcePluginServerGRPC is used by plugins to map GRPC calls from the clients to
-// methods of the DataSource interface.
-type DataSourcePluginServerGRPC struct {
-	Impl DataSource
-}
-
-func (c *DataSourcePluginServerGRPC) GetTrustZones(context.Context, *cofidectl_proto.GetTrustZonesRequest) (*cofidectl_proto.GetTrustZonesResponse, error) {
-	v, err := c.Impl.GetTrustZones()
-	return &cofidectl_proto.GetTrustZonesResponse{TrustZones: v}, err
-}
-
-func (c *DataSourcePluginServerGRPC) CreateTrustZone(context.Context, *cofidectl_proto.CreateTrustZoneRequest) (*cofidectl_proto.CreateTrustZoneResponse, error) {
-	v, err := c.Impl.CreateTrustZone()
-	return &cofidectl_proto.CreateTrustZoneResponse{TrustZone: v}, err
-}
-
-func (c *DataSourcePluginServerGRPC) CreateAttestationPolicy(context.Context, *cofidectl_proto.CreateAttestationPolicyRequest) (*cofidectl_proto.CreateAttestationPolicyResponse, error) {
-	v, err := c.Impl.CreateAttestationPolicy()
-	return &cofidectl_proto.CreateAttestationPolicyResponse{AttestationPolicy: v}, err
-}
+*/
