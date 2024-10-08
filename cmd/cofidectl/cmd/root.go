f75b03bc40529b74c935246396a4e8ce31ff5610
Jason Costello <jason@cofide.io>
2024-10-08 12:14:53 +0100

    [From review] Remove ODPServiceName const

--- cmd/cofidectl/cmd/root.go
+++ cmd/cofidectl/cmd/root.go
@@ -4,10 +4,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	spiffeOdpServiceName = "spiffe-oidc-discovery-provider"
-)
-
 var longDesc = `cofidectl - Workload identity for hybrid and multi-cloud security`
 
 func NewRootCmd(args []string) (*cobra.Command, error) {
