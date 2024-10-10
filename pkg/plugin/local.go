c23f6c31c7259f54b77ebce8b839e892ece068ab
nialdaly <nial.daly@outlook.com>
2024-10-10 19:33:17 +0100

    update federation in schemaCue string

--- pkg/plugin/local.go
+++ pkg/plugin/local.go
@@ -35,7 +35,7 @@ const schemaCue = `
 	pod_annotation_value: string
 }
 
-#federation: {
+#Federation: {
 	left: string
 	right: string
 }
@@ -45,7 +45,6 @@ const schemaCue = `
 	trust_zones: [...#TrustZone]
 	attestation_policy: [...#AttestationPolicy]
 	federation: [...#Federation]
-
 }
 
 config: #Config
