85af9ad751b0db29d3fcc027f4f36c1995ceb5b6
nialdaly <nial.daly@outlook.com>
2024-10-11 09:18:26 +0100

    spire-agent and spire-server sensible defaults started

--- internal/pkg/provider/helm/helm.go
+++ internal/pkg/provider/helm/helm.go
@@ -167,6 +167,20 @@ func (g *HelmValuesGenerator) GenerateValues() (map[string]interface{}, error) {
 	valuesCUE = valuesCUE.FillPath(cue.ParsePath("global.spire.clusterName"), trustZones[0].KubernetesCluster)
 	valuesCUE = valuesCUE.FillPath(cue.ParsePath("global.spire.trustDomain"), trustZones[0].TrustDomain)
 
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath("global.spire.recommendations.create"), true)
+
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath("global.installAndUpgradeHooks.enabled"), false)
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath("global.deleteHooks.enabled"), false)
+
+	// NOTE: https://github.com/cue-lang/cue/issues/358
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath(`"spire-agent"."fullnameOverride"`), "spire-agent")
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath(`"spire-agent"."logLevel"`), "DEBUG")
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath(`"spire-agent"."server"."address"`), fmt.Sprintf("%s.%s", "spire-server", "spire"))
+
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath(`"spire-server"."caKeyType"`), "rsa-2048")
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath(`"spire-server"."caTTL"`), "12h")
+	valuesCUE = valuesCUE.FillPath(cue.ParsePath(`"spire-server"."fullnameOverride"`), "spire-server")
+
 	valuesJSON, err := valuesCUE.MarshalJSON()
 	if err != nil {
 		// TODO: Improve error messaging.
