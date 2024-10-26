f5f3dbf402349dea97c428f3851429f1aaca2c62
Matthew Bates <matt@cofide.io>
2024-10-26 15:51:14 +0100

    add initial support for secret (metadata) discovery

--- internal/pkg/workload/workload.go
+++ internal/pkg/workload/workload.go
@@ -2,9 +2,12 @@ package workload
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	kubeutil "github.com/cofide/cofidectl/internal/pkg/kube"
 	"github.com/cofide/cofidectl/internal/pkg/spire"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -14,6 +17,13 @@ type Workload struct {
 	SPIFFEID  string
 	Status    string
 	Type      string
+	Secrets   []*WorkloadSecretMetadata
+}
+
+type WorkloadSecretMetadata struct {
+	Name string
+	Type string
+	Age  time.Duration
 }
 
 // GetRegisteredWorkloads will find all workloads that are registered with the WI platform
@@ -79,6 +89,14 @@ func GetUnregisteredWorkloads(kubeCfgFile string, kubeContext string, secretDisc
 		return nil, err
 	}
 
+	var secrets *v1.SecretList
+	if secretDiscovery {
+		secrets, err = client.Clientset.CoreV1().Secrets("").List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	unregisteredWorkloads := []Workload{}
 
 	for _, pod := range pods.Items {
@@ -96,9 +114,62 @@ func GetUnregisteredWorkloads(kubeCfgFile string, kubeContext string, secretDisc
 				Type:      "Pod",
 			}
 
+			// add related secrets (metadata) if secret discovery is enabled
+			if secretDiscovery {
+				findRelatedSecrets(&pod, unregisteredWorkload, secrets)
+
+			}
+
 			unregisteredWorkloads = append(unregisteredWorkloads, *unregisteredWorkload)
 		}
 	}
 
 	return unregisteredWorkloads, nil
+}
+
+func findRelatedSecrets(pod *v1.Pod, workload *Workload, secrets *v1.SecretList) {
+	for _, secret := range secrets.Items {
+		age := time.Since(secret.CreationTimestamp.Time)
+		// Consider secrets older than 30 days as long-lived
+		if age > 30*24*time.Hour {
+			key := fmt.Sprintf("%s/%s", secret.Namespace, secret.Name)
+			workload.Secrets = append(workload.Secrets, &WorkloadSecretMetadata{
+				Name: key,
+				Type: "secret",
+				Age:  age,
+			})
+		}
+	}
+
+	for _, volume := range pod.Spec.Volumes {
+		if volume.Secret != nil {
+			key := fmt.Sprintf("%s/%s", pod.Namespace, volume.Secret.SecretName)
+			workload.Secrets = append(workload.Secrets, &WorkloadSecretMetadata{
+				Name: key,
+				Type: "volume",
+			})
+		}
+	}
+
+	// Check secrets used in environment variables
+	for _, container := range pod.Spec.Containers {
+		for _, env := range container.EnvFrom {
+			if env.SecretRef != nil {
+				key := fmt.Sprintf("%s/%s", pod.Namespace, env.SecretRef.Name)
+				workload.Secrets = append(workload.Secrets, &WorkloadSecretMetadata{
+					Name: key,
+					Type: "env",
+				})
+			}
+		}
+		for _, env := range container.Env {
+			if env.ValueFrom != nil && env.ValueFrom.SecretKeyRef != nil {
+				key := fmt.Sprintf("%s/%s", pod.Namespace, env.ValueFrom.SecretKeyRef.Name)
+				workload.Secrets = append(workload.Secrets, &WorkloadSecretMetadata{
+					Name: key,
+					Type: "env",
+				})
+			}
+		}
+	}
 }
