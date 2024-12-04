99bd3a34c5e3f9c382654bc7743bf05265c99c0e
Mark Goddard <mark@cofide.io>
2024-12-04 09:56:47 +0000

    Implement full data source gRPC client and service
    Implements all data source gRPC service RPCs for the client and service.
    
    Adds a type check to ensure that the client implements the DataSource
    interface.
    
    Fixes: #59

--- pkg/plugin/grpc.go
+++ pkg/plugin/grpc.go
@@ -13,6 +13,9 @@ import (
 	trust_zone_proto "github.com/cofide/cofide-api-sdk/gen/go/proto/trust_zone/v1alpha1"
 )
 
+// Type check to ensure DataSourcePluginClientGRPC implements DataSource.
+var _ DataSource = &DataSourcePluginClientGRPC{}
+
 // DataSourcePluginClientGRPC is used by clients (main application) to translate the
 // DataSource interface of plugins to GRPC calls.
 type DataSourcePluginClientGRPC struct {
@@ -21,13 +24,17 @@ type DataSourcePluginClientGRPC struct {
 }
 
 func (c *DataSourcePluginClientGRPC) Validate() error {
-	// Unimplemented.
-	return nil
+	_, err := c.client.Validate(c.ctx, &cofidectl_proto.ValidateRequest{})
+	return err
 }
 
 func (c *DataSourcePluginClientGRPC) GetTrustZone(name string) (*trust_zone_proto.TrustZone, error) {
-	// Unimplemented.
-	return nil, nil
+	resp, err := c.client.GetTrustZone(c.ctx, &cofidectl_proto.GetTrustZoneRequest{Name: &name})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.TrustZone, nil
 }
 
 func (c *DataSourcePluginClientGRPC) ListTrustZones() ([]*trust_zone_proto.TrustZone, error) {
@@ -40,51 +47,83 @@ func (c *DataSourcePluginClientGRPC) ListTrustZones() ([]*trust_zone_proto.Trust
 }
 
 func (c *DataSourcePluginClientGRPC) AddTrustZone(trustZone *trust_zone_proto.TrustZone) (*trust_zone_proto.TrustZone, error) {
-	// Unimplemented.
-	return nil, nil
+	resp, err := c.client.AddTrustZone(c.ctx, &cofidectl_proto.AddTrustZoneRequest{TrustZone: trustZone})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.TrustZone, nil
 }
 
-func (c *DataSourcePluginClientGRPC) UpdateTrustZone(*trust_zone_proto.TrustZone) error {
-	// Unimplemented.
-	return nil
+func (c *DataSourcePluginClientGRPC) UpdateTrustZone(trustZone *trust_zone_proto.TrustZone) error {
+	_, err := c.client.UpdateTrustZone(c.ctx, &cofidectl_proto.UpdateTrustZoneRequest{TrustZone: trustZone})
+	return err
 }
 
-func (c *DataSourcePluginClientGRPC) AddAttestationPolicy(*attestation_policy_proto.AttestationPolicy) (*attestation_policy_proto.AttestationPolicy, error) {
-	// Unimplemented.
-	return nil, nil
+func (c *DataSourcePluginClientGRPC) AddAttestationPolicy(policy *attestation_policy_proto.AttestationPolicy) (*attestation_policy_proto.AttestationPolicy, error) {
+	resp, err := c.client.AddAttestationPolicy(c.ctx, &cofidectl_proto.AddAttestationPolicyRequest{Policy: policy})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Policy, nil
 }
 
-func (c *DataSourcePluginClientGRPC) GetAttestationPolicy(string) (*attestation_policy_proto.AttestationPolicy, error) {
-	// Unimplemented.
-	return nil, nil
+func (c *DataSourcePluginClientGRPC) GetAttestationPolicy(name string) (*attestation_policy_proto.AttestationPolicy, error) {
+	resp, err := c.client.GetAttestationPolicy(c.ctx, &cofidectl_proto.GetAttestationPolicyRequest{Name: &name})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Policy, nil
 }
 
 func (c *DataSourcePluginClientGRPC) ListAttestationPolicies() ([]*attestation_policy_proto.AttestationPolicy, error) {
-	// Unimplemented.
-	return nil, nil
+	resp, err := c.client.ListAttestationPolicies(c.ctx, &cofidectl_proto.ListAttestationPoliciesRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Policies, nil
 }
 
-func (c *DataSourcePluginClientGRPC) AddAPBinding(*ap_binding_proto.APBinding) (*ap_binding_proto.APBinding, error) {
-	// Unimplemented.
-	return nil, nil
+func (c *DataSourcePluginClientGRPC) AddAPBinding(binding *ap_binding_proto.APBinding) (*ap_binding_proto.APBinding, error) {
+	resp, err := c.client.AddAPBinding(c.ctx, &cofidectl_proto.AddAPBindingRequest{Binding: binding})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Binding, nil
 }
 
-func (c *DataSourcePluginClientGRPC) DestroyAPBinding(*ap_binding_proto.APBinding) error {
-	// Unimplemented.
-	return nil
+func (c *DataSourcePluginClientGRPC) DestroyAPBinding(binding *ap_binding_proto.APBinding) error {
+	_, err := c.client.DestroyAPBinding(c.ctx, &cofidectl_proto.DestroyAPBindingRequest{Binding: binding})
+	return err
 }
 
-func (c *DataSourcePluginClientGRPC) AddFederation(*federation_proto.Federation) (*federation_proto.Federation, error) {
-	// Unimplemented.
-	return nil, nil
+func (c *DataSourcePluginClientGRPC) AddFederation(federation *federation_proto.Federation) (*federation_proto.Federation, error) {
+	resp, err := c.client.AddFederation(c.ctx, &cofidectl_proto.AddFederationRequest{Federation: federation})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Federation, nil
 }
 
 func (c *DataSourcePluginClientGRPC) ListFederations() ([]*federation_proto.Federation, error) {
-	// Unimplemented.
-	return nil, nil
+	resp, err := c.client.ListFederations(c.ctx, &cofidectl_proto.ListFederationsRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Federations, nil
 }
 
 func (c *DataSourcePluginClientGRPC) ListFederationsByTrustZone(string) ([]*federation_proto.Federation, error) {
-	// Unimplemented.
-	return nil, nil
+	resp, err := c.client.ListFederationsByTrustZone(c.ctx, &cofidectl_proto.ListFederationsByTrustZoneRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Federations, nil
 }
