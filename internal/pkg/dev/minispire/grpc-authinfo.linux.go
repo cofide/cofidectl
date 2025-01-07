30b1b3fb66e59a55df37ec554011c8af1cc8738e
Maartje Eyskens <maartje@eyskens.me>
2025-01-07 10:58:13 +0100

    run getProcessInfo only on linux
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

---
+++ internal/pkg/dev/minispire/grpc-authinfo.linux.go
@@ -0,0 +1,17 @@
+//go:build linux
+
+package minispire
+
+import (
+	"log"
+	"syscall"
+)
+
+func getProcessInfo(fd uintptr) (int32, uint32, uint32) {
+	cred, err := syscall.GetsockoptUcred(int(fd), syscall.SOL_SOCKET, syscall.SO_PEERCRED)
+	if err != nil {
+		log.Printf("unable to get peer credentials: %v", err)
+		return 0, 0, 0
+	}
+	return int32(cred.Pid), uint32(cred.Uid), uint32(cred.Gid)
+}
