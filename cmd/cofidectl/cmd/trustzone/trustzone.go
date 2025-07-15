ffb3a3cba9725522ffbab213046d26ad9087774d
Jason Costello <jason@cofide.io>
2025-07-15 16:31:09 +0100

    Amend docstring

--- cmd/cofidectl/cmd/trustzone/trustzone.go
+++ cmd/cofidectl/cmd/trustzone/trustzone.go
@@ -174,7 +174,7 @@ func (c *TrustZoneCommand) GetAddCommand() *cobra.Command {
 	f.StringVar(&opts.trustDomain, "trust-domain", "", "Trust domain to use for this trust zone")
 	f.StringVar(&opts.kubernetesCluster, "kubernetes-cluster", "", "Kubernetes cluster associated with this trust zone")
 	f.StringVar(&opts.kubernetesClusterOIDCIssuerURL, "kubernetes-oidc-issuer", "", "OIDC issuer URL for the Kubernetes cluster")
-	f.StringVar(&opts.kubernetesClusterCACert, "kubernetes-ca-cert", "", "CA certificate of the Kubernetes cluster, used for TLS during OIDC validation")
+	f.StringVar(&opts.kubernetesClusterCACert, "kubernetes-ca-cert", "", "Path to the CA certificate of the Kubernetes cluster, used for TLS during OIDC validation")
 	f.StringVar(&opts.context, "kubernetes-context", "", "Kubernetes context to use for this trust zone")
 	f.StringVar(&opts.profile, "profile", "kubernetes", "Cofide profile used in the installation (e.g. kubernetes, istio)")
 	f.StringVar(&opts.jwtIssuer, "jwt-issuer", "", "JWT issuer to use for this trust zone")
