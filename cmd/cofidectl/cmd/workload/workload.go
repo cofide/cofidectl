8e4147f8506033870d925726442711ca4c2fe6e5
Matthew Bates <matt@cofide.io>
2025-09-03 16:39:31 +0100

    add config option for the SPIFFE socket path

--- cmd/cofidectl/cmd/workload/workload.go
+++ cmd/cofidectl/cmd/workload/workload.go
@@ -119,10 +119,11 @@ This command will display the status of workloads in the Cofide configuration st
 `
 
 type StatusOpts struct {
-	podName       string
-	namespace     string
-	trustZone     string
-	wlVolumeMount string
+	podName                 string
+	namespace               string
+	trustZone               string
+	spiffeSocketVolumeMount string
+	spiffeSocketPath        string
 }
 
 func (w *WorkloadCommand) GetStatusCommand() *cobra.Command {
@@ -151,7 +152,8 @@ func (w *WorkloadCommand) GetStatusCommand() *cobra.Command {
 	f.StringVar(&opts.podName, "pod-name", "", "Pod name for the workload")
 	f.StringVar(&opts.namespace, "namespace", "", "Namespace for the workload")
 	f.StringVar(&opts.trustZone, "trust-zone", "", "Trust zone for the workload")
-	f.StringVar(&opts.wlVolumeMount, "workload-api-volume-mount", "spiffe-workload-api", "The volume mount for the Workload API socket")
+	f.StringVar(&opts.spiffeSocketVolumeMount, "spiffe-socket-volume-mount", "spiffe-workload-api", "The volume mount for the SPIFFE socket (UDS)")
+	f.StringVar(&opts.spiffeSocketPath, "spiffe-socket-path", "spiffe-workload-api", "The path to the SPIFFE socket (UDS)")
 
 	cobra.CheckErr(cmd.MarkFlagRequired("pod-name"))
 	cobra.CheckErr(cmd.MarkFlagRequired("namespace"))
@@ -176,7 +178,7 @@ func (w *WorkloadCommand) status(ctx context.Context, ds datasource.DataSource, 
 		return err
 	}
 
-	statusCh, dataCh := getWorkloadStatus(ctx, client, opts.podName, opts.namespace, opts.wlVolumeMount)
+	statusCh, dataCh := getWorkloadStatus(ctx, client, opts.podName, opts.namespace, opts.spiffeSocketVolumeMount, opts.spiffeSocketPath)
 
 	// Create a spinner to display whilst the debug container is created and executed and logs retrieved
 	if err := statusspinner.WatchProvisionStatus(ctx, statusCh, false); err != nil {
@@ -241,14 +243,14 @@ func renderRegisteredWorkloads(ctx context.Context, ds datasource.DataSource, ku
 	return nil
 }
 
-func getWorkloadStatus(ctx context.Context, client *kubeutil.Client, podName, namespace, wlVolumeMount string) (<-chan *provisionpb.Status, chan string) {
+func getWorkloadStatus(ctx context.Context, client *kubeutil.Client, podName, namespace, spiffeSocketPath, spiffeSocketVolumeMount string) (<-chan *provisionpb.Status, chan string) {
 	statusCh := make(chan *provisionpb.Status)
 	dataCh := make(chan string, 1)
 
 	go func() {
 		defer close(statusCh)
 		defer close(dataCh)
-		workload.GetStatus(ctx, statusCh, dataCh, client, podName, namespace, wlVolumeMount)
+		workload.GetStatus(ctx, statusCh, dataCh, client, podName, namespace, spiffeSocketPath, spiffeSocketVolumeMount)
 	}()
 
 	return statusCh, dataCh
--- internal/pkg/workload/workload.go
+++ internal/pkg/workload/workload.go
@@ -20,7 +20,7 @@ import (
 )
 
 const debugContainerNamePrefix = "cofidectl-debug"
-const debugContainerImage = "ghcr.io/cofide/cofidectl-debug-container:v0.2.1"
+const debugContainerImage = "ghcr.io/cofide/cofidectl-debug-container:v0.2.2-dev"
 
 type Workload struct {
 	Name             string
@@ -145,7 +145,7 @@ func GetUnregisteredWorkloads(ctx context.Context, kubeCfgFile string, kubeConte
 	return unregisteredWorkloads, nil
 }
 
-func GetStatus(ctx context.Context, statusCh chan<- *provisionpb.Status, dataCh chan string, client *kubeutil.Client, podName, namespace, wlVolumeMount string) {
+func GetStatus(ctx context.Context, statusCh chan<- *provisionpb.Status, dataCh chan string, client *kubeutil.Client, podName, namespace, spiffeSocketPath, spiffeSocketVolumeMount string) {
 	debugContainerName := fmt.Sprintf("%s-%s", debugContainerNamePrefix, rand.String(5))
 
 	statusCh <- provision.StatusOk(
@@ -153,7 +153,7 @@ func GetStatus(ctx context.Context, statusCh chan<- *provisionpb.Status, dataCh 
 		fmt.Sprintf("Waiting for ephemeral debug container to be created in %s", podName),
 	)
 
-	if err := createDebugContainer(ctx, client, podName, namespace, wlVolumeMount, debugContainerName); err != nil {
+	if err := createDebugContainer(ctx, client, podName, namespace, spiffeSocketPath, spiffeSocketVolumeMount, debugContainerName); err != nil {
 		statusCh <- provision.StatusError(
 			"Creating",
 			fmt.Sprintf("Failed waiting for ephemeral debug container to be created in %s", podName),
@@ -193,7 +193,7 @@ func GetStatus(ctx context.Context, statusCh chan<- *provisionpb.Status, dataCh 
 	)
 }
 
-func createDebugContainer(ctx context.Context, client *kubeutil.Client, podName string, namespace string, wlVolumeMount string, debugContainerName string) error {
+func createDebugContainer(ctx context.Context, client *kubeutil.Client, podName, namespace, spiffeSocketPath, spiffeSocketVolumeName, debugContainerName string) error {
 	pod, err := client.Clientset.CoreV1().Pods(namespace).Get(ctx, podName, metav1.GetOptions{})
 	if err != nil {
 		return err
@@ -209,11 +209,18 @@ func createDebugContainer(ctx context.Context, client *kubeutil.Client, podName 
 			VolumeMounts: []v1.VolumeMount{
 				{
 					ReadOnly:  true,
-					Name:      wlVolumeMount,
-					MountPath: fmt.Sprintf("/%s", wlVolumeMount),
+					Name:      spiffeSocketVolumeName,
+					MountPath: fmt.Sprintf("/%s", spiffeSocketVolumeName),
 				}},
 		},
 		TargetContainerName: pod.Spec.Containers[0].Name,
+	}
+
+	if spiffeSocketPath != "" {
+		debugContainer.Env = append(debugContainer.Env, v1.EnvVar{
+			Name:  "SPIFFE_ENDPOINT_SOCKET",
+			Value: spiffeSocketPath,
+		})
 	}
 
 	pod.Spec.EphemeralContainers = append(pod.Spec.EphemeralContainers, debugContainer)
